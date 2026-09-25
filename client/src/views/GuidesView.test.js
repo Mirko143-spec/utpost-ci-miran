06a3816c1ef461190f48caea8db48ba36a709435
mirko143 <miran123125@gmail.com>
2026-09-25 13:11:10 +0200

    test: fixed the problems

--- client/src/views/GuidesView.test.js
+++ client/src/views/GuidesView.test.js
@@ -1,7 +1,7 @@
 // Röktest: sviten får vara ett enda test den här veckan (M1). Det ska bevisa att
 // pipelinen kör tester – inte att appen är korrekt. Riktiga tester kommer i M2.
 import { describe, it, expect, vi } from 'vitest'
-import { mount } from '@vue/test-utils'
+import { mount, flushPromises } from '@vue/test-utils'
 import GuidesView from './GuidesView.vue'
 
 vi.mock('../api.js', () => ({
@@ -21,6 +21,7 @@ describe('GuidesView', () => {
     const wrapper = mount(GuidesView, {
       global: { stubs: { RouterLink: { template: '<a><slot /></a>' } } },
     })
+    await flushPromises()
     expect(wrapper.text()).toContain('Kebnekaise')
     expect(wrapper.text()).toContain('1 av 1')
   })
